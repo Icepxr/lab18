b7f49d9507634b1ae7d6132764f4eeded5d677a4
Daiyaz <uchihachikk3@gmail.com>
2025-02-11 13:17:42 +0700

    Refactor overlap function and improve rectangle structure formatting

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2) {
+    double x1 = max(r1.x, r2.x);
+    double x2 = min(r1.x + r1.w, r2.x + r2.w);
+    double y1 = min(r1.y, r2.y);
+    double y2 = max(r1.y - r1.h, r2.y - r2.h);
+    
+    double width = x2 - x1;
+    double height = y1 - y2;
+    
+    if (width > 0 && height > 0) {
+        return width * height;
+    } else {
+        return 0.0;
+    }
+}
 
 
-
+int main() {
+    Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,1.2,2.5};	
+	cout << overlap(R1,R2);	
+    return 0;
 }
