55275f1dfd73f381a01c6a903992dc100d00408d
Daiyaz <uchihachikk3@gmail.com>
2025-02-11 13:18:15 +0700

    Enhance overlap function to accept rectangle pointers and calculate intersection area

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,27 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect* R1,Rect* R2){
+	double x1 = max(R1->x,R2->x);
+	double x2 = min(R1->x+R1->w,R2->x+R2->w);
+	double y1 = min(R1->y,R2->y);
+	double y2 = max(R1->y-R1->h,R2->y-R2->h);
+	
+	double width = x2-x1;
+	double height = y1-y2;
+	
+	if(width > 0 && height > 0){
+		return width*height;
+	}else{
+		return 0.0;
+	}
 
 
 
+}
+int main() {
+    Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,1.2,2.5};	
+	cout << overlap(&R1,&R2);	
+    return 0;
 }
